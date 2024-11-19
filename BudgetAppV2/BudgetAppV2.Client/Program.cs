e924395c44b93fa2809e7c1dc2d57f9bc9bcfc6e
Jozsef Simon <98556252+jozsefsimon22@users.noreply.github.com>
2024-11-19 22:48:19 +0100

    Added Radzen

---
+++ BudgetAppV2/BudgetAppV2.Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddRadzenCookieThemeService(options =>
+{
+    options.Name = "MyApplicationTheme"; // The name of the cookie
+    options.Duration = TimeSpan.FromDays(365); // The duration of the cookie
+});
+
+await builder.Build().RunAsync();
---
+++ BudgetAppV2/BudgetAppV2/Program.cs
@@ -0,0 +1,44 @@
+using BudgetAppV2.Client.Pages;
+using BudgetAppV2.Components;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddRadzenCookieThemeService(options =>
+{
+    options.Name = "MyApplicationTheme"; // The name of the cookie
+    options.Duration = TimeSpan.FromDays(365); // The duration of the cookie
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
