769f2ecdc23621cbe6d914acebece0f83e4fab92
Jozsef Simon <98556252+jozsefsimon22@users.noreply.github.com>
2025-04-19 12:59:15 +0100

    Added migration files

---
+++ BudgetAppV2/BudgetAppV2/Migrations/20250321225506_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetAppV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FinancialTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TransactionType = table.Column<int>(type: "integer", nullable: false),
+                    IsRecurring = table.Column<bool>(type: "boolean", nullable: false),
+                    TransactionFrequency = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CurrentAmount = table.Column<double>(type: "double precision", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialTransactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinancialTransactionHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    FinancialTransactionId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialTransactionHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinancialTransactionHistories_FinancialTransactions_Financi~",
+                        column: x => x.FinancialTransactionId,
+                        principalTable: "FinancialTransactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinancialTransactionHistories_FinancialTransactionId",
+                table: "FinancialTransactionHistories",
+                column: "FinancialTransactionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialTransactionHistories");
+
+            migrationBuilder.DropTable(
+                name: "FinancialTransactions");
+        }
+    }
+}
---
+++ BudgetAppV2/BudgetAppV2/Migrations/20250325200953_DataSeeding.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BudgetAppV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "FinancialTransactions",
+                columns: new[] { "Id", "CurrentAmount", "EndDate", "IsRecurring", "Name", "StartDate", "TransactionFrequency", "TransactionType" },
+                values: new object[,]
+                {
+                    { new Guid("0993573a-9d10-46d2-887a-5e29b3b32308"), 150.0, new DateOnly(2024, 5, 20), false, "Concert Tickets", new DateOnly(2024, 5, 20), 3, 1 },
+                    { new Guid("6ea81ae4-3bf4-4099-b844-72348a47bb0b"), 1200.0, new DateOnly(2024, 2, 10), false, "New Laptop", new DateOnly(2024, 2, 10), 3, 1 },
+                    { new Guid("73489b93-b090-4746-bbcc-b99c729f36ab"), 25.0, null, true, "Gym Membership", new DateOnly(2023, 3, 1), 1, 1 },
+                    { new Guid("b1501478-fdba-49aa-8599-e23cdd8ad3b1"), 15.99, null, true, "Netflix Subscription", new DateOnly(2023, 1, 1), 1, 1 },
+                    { new Guid("c02ee91f-7c3a-42c6-ba14-7a2df4934a3d"), 2500.0, null, true, "Salary", new DateOnly(2022, 6, 15), 1, 0 },
+                    { new Guid("e36e7193-fd9d-4b53-929b-46dd90dc7317"), 800.0, null, false, "Freelance Project", new DateOnly(2024, 1, 15), 3, 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("0993573a-9d10-46d2-887a-5e29b3b32308"));
+
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("6ea81ae4-3bf4-4099-b844-72348a47bb0b"));
+
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("73489b93-b090-4746-bbcc-b99c729f36ab"));
+
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("b1501478-fdba-49aa-8599-e23cdd8ad3b1"));
+
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("c02ee91f-7c3a-42c6-ba14-7a2df4934a3d"));
+
+            migrationBuilder.DeleteData(
+                table: "FinancialTransactions",
+                keyColumn: "Id",
+                keyValue: new Guid("e36e7193-fd9d-4b53-929b-46dd90dc7317"));
+        }
+    }
+}
